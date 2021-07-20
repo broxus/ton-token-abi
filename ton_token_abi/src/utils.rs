33ac7e8950c4f7b86e362de62aea5c880781f173
Alexey Pashinov <pashinov93@gmail.com>
2021-07-20 13:42:18 +0300

    Pin ton-labs-abi v1.0.1; fix warnings

--- ton_token_abi/src/utils.rs
+++ ton_token_abi/src/utils.rs
@@ -1,6 +1,6 @@
 use crate::symbol::ABI;
 
-pub fn is_abi(attrs: &Vec<syn::Attribute>) -> bool {
+pub fn is_abi(attrs: &[syn::Attribute]) -> bool {
     for attr in attrs {
         if attr.path.segments.len() == 1 && attr.path.segments[0].ident == ABI {
             return true;
